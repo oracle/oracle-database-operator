a58f1338b97f3e9de78be95304e0de6644e00f70
Mahi0911 <martha.mahipal@oracle.com>
2021-12-08 11:41:03 +0530

    extract sid,pdb,edition for prebuiltdb

--- commons/database/constants.go
+++ commons/database/constants.go
@@ -422,4 +422,4 @@ const SetApexUsers string = "\numask 177" +
 	"\numask 022"
 
 // Get Sid, Pdbname, Edition for prebuilt db
-const GetSidPdbEditionCMD string = "if [ -f ${ORACLE_BASE}/oradata/dbconfig/$ORACLE_SID/.docker_standard ]; then echo \"$ORACLE_SID,$ORACLE_PDB,Standard\"; else echo \"$ORACLE_SID,$ORACLE_PDB,Enterprise\"; fi;"
+const GetSidPdbEditionCMD string = "if [ -f ${ORACLE_BASE}/oradata/dbconfig/$ORACLE_SID/.docker_standard ]; then echo \"$ORACLE_SID,$ORACLE_PDB,Standard,Edition\"; else echo \"$ORACLE_SID,$ORACLE_PDB,Enterprise,Edition\"; fi;"
