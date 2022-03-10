edb06936114c108762a28ecb71661865abb25f95
harayuanwang <ting-lan.wang@oracle.com>
2022-03-10 09:18:48 -0500

    fix minor errors

--- controllers/database/suite_test.go
+++ controllers/database/suite_test.go
@@ -87,9 +87,6 @@ var _ = BeforeSuite(func(done Done) {
 	err = databasev1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = databasev1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
 	// +kubebuilder:scaffold:scheme
 
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
