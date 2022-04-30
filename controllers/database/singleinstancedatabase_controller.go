24c7739f30011f8214c789c03ece0ec058885891
Yunus Qureshi <write2yunus@gmail.com>
2022-04-30 15:12:54 +0530

    Fix claim selector

--- controllers/database/singleinstancedatabase_controller.go
+++ controllers/database/singleinstancedatabase_controller.go
@@ -839,7 +839,7 @@ func (r *SingleInstanceDatabaseReconciler) instantiatePVCSpec(m *dbapi.SingleIns
 			},
 			StorageClassName: &m.Spec.Persistence.StorageClass,
 			Selector: func() *metav1.LabelSelector {
-				if m.Spec.Persistence.StorageClass == "oci-bv" {
+				if m.Spec.Persistence.StorageClass != "oci" {
 					return nil
 				}
 				return &metav1.LabelSelector{
