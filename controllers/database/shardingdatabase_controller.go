aab9038f78d399c58efc21c5dabf45c135db91cb
geela.rajahimansh@oracle.com <grajahim@phoenix731075.dev3sub4phx.databasede3phx.oraclevcn.com>
2026-03-11 04:57:30 +0000

    error fix

--- controllers/database/shardingdatabase_controller.go
+++ controllers/database/shardingdatabase_controller.go
@@ -216,7 +216,7 @@ func (r *ShardingDatabaseReconciler) Reconcile(ctx context.Context, req ctrl.Req
 		return result, err
 	}
 
-	// cleanup any shard resources that are no longer present in desired spec
+	// cleanup on scale in
 	if cerr := r.cleanupOrphanShardResources(instance); cerr != nil {
 		shardingv1.LogMessages("INFO", "Failed to cleanup orphan shard resources: "+cerr.Error(), nil, instance, r.Log)
 		result = resultQ
@@ -2689,52 +2689,49 @@ func (r *ShardingDatabaseReconciler) cleanupOrphanShardResources(instance *datab
 		}
 	}
 
-	sfList := &appsv1.StatefulSetList{}
-	listOpts := []client.ListOption{
-		client.InNamespace(instance.Namespace),
-		client.MatchingLabels(map[string]string{
-			"app":      "OracleSharding",
-			"oralabel": instance.Name,
-			"type":     "Shard",
-		}),
-	}
+	shardingv1.LogMessages("INFO", fmt.Sprintf("cleanupOrphanShardResources desired shards: %+v", desired), nil, instance, r.Log)
 
-	if err := r.Client.List(context.TODO(), sfList, listOpts...); err != nil {
+	sfList := &appsv1.StatefulSetList{}
+	if err := r.Client.List(context.TODO(), sfList, client.InNamespace(instance.Namespace)); err != nil {
 		return err
 	}
 
 	for i := range sfList.Items {
 		sf := &sfList.Items[i]
-		shardName := strings.TrimSpace(sf.Name)
+		name := strings.TrimSpace(sf.Name)
 
-		if desired[shardName] {
+		// only shard statefulsets owned by this CR
+		lbls := sf.GetLabels()
+		if lbls["type"] != "Shard" {
+			continue
+		}
+		if lbls["oralabel"] != instance.Name {
 			continue
 		}
 
-		shardingv1.LogMessages(
-			"INFO",
-			"Found orphan shard resources for "+shardName+"; deleting StatefulSet/Service/PVC",
-			nil,
-			instance,
-			r.Log,
-		)
+		if desired[name] {
+			shardingv1.LogMessages("INFO", "cleanupOrphanShardResources keeping shard "+name, nil, instance, r.Log)
+			continue
+		}
+
+		shardingv1.LogMessages("INFO", "cleanupOrphanShardResources deleting orphan shard "+name, nil, instance, r.Log)
 
 		// delete statefulset
 		if err := r.Client.Delete(context.Background(), sf); err != nil && !errors.IsNotFound(err) {
 			return err
 		}
 
-		// delete local service
-		svcFound, err := shardingv1.CheckSvc(shardName, instance, r.Client)
+		// delete service
+		svcFound, err := shardingv1.CheckSvc(name, instance, r.Client)
 		if err == nil {
 			if derr := r.Client.Delete(context.Background(), svcFound); derr != nil && !errors.IsNotFound(derr) {
 				return derr
 			}
 		}
 
-		// delete external service if any
+		// delete external service if present
 		if instance.Spec.IsExternalSvc {
-			svcExt, err := shardingv1.CheckSvc(shardName+strconv.FormatInt(int64(0), 10)+"-svc", instance, r.Client)
+			svcExt, err := shardingv1.CheckSvc(name+strconv.FormatInt(int64(0), 10)+"-svc", instance, r.Client)
 			if err == nil {
 				if derr := r.Client.Delete(context.Background(), svcExt); derr != nil && !errors.IsNotFound(derr) {
 					return derr
@@ -2742,24 +2739,24 @@ func (r *ShardingDatabaseReconciler) cleanupOrphanShardResources(instance *datab
 			}
 		}
 
-		// delete pvc if enabled
+		// delete pvc
 		if instance.Spec.IsDeleteOraPvc && len(instance.Spec.StorageClass) > 0 {
-			pvcName := shardName + "-oradata-vol4-" + shardName + "-0"
+			pvcName := name + "-oradata-vol4-" + name + "-0"
 			if err := shardingv1.DelPvc(pvcName, instance, r.Client, r.Log); err != nil && !errors.IsNotFound(err) {
 				return err
 			}
 		}
 
-		// best effort status cleanup
-		if instance.Status.Gsm.Shards != nil {
-			delete(instance.Status.Gsm.Shards, shardName)
-		}
+		// cleanup shard status map
 		if instance.Status.Shard != nil {
 			for k := range instance.Status.Shard {
-				if strings.HasPrefix(k, shardName+"_") {
+				if strings.HasPrefix(k, name+"_") {
 					delete(instance.Status.Shard, k)
 				}
 			}
+		}
+		if instance.Status.Gsm.Shards != nil {
+			delete(instance.Status.Gsm.Shards, name)
 		}
 	}
 
